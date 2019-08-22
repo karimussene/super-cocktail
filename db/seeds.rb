dc4b3014f8a3f05fe0cf80d3a5e68fe778011d26
rikas <oterosantos@gmail.com>
2019-08-22 19:17:24 +0100

    Code by the end of evening course

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+
+puts 'Creating ingredients'
+
+Ingredient.create(name: 'lemon')
+Ingredient.create(name: 'ice')
+Ingredient.create(name: 'mint leaves')
+Ingredient.create(name: 'rum')
+Ingredient.create(name: 'sugar')
+
+puts 'Done'
