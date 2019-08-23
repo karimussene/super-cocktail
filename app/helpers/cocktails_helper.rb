a6874b8827842d7358bb61236a5c5154544ca93b
rikas <oterosantos@gmail.com>
2019-08-23 10:34:32 +0100

    Add the uploaded photo to show page

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -1,9 +1,11 @@
 module CocktailsHelper
   def photo_or_fallback(cocktail)
     if cocktail.photo.present?
+      # cl_image_tag # => generates a <img> tag
       cl_image_path(cocktail.photo)
     else
       asset_path('placeholder.jpg')
+      # image_tag # => generates a <img> tag
     end
   end
 end
