4d23790f3124ff8f315ae8729499421eb902c50c
rikas <oterosantos@gmail.com>
2019-08-23 10:29:00 +0100

    Add photo upload

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -1,2 +1,9 @@
 module CocktailsHelper
+  def photo_or_fallback(cocktail)
+    if cocktail.photo.present?
+      cl_image_path(cocktail.photo)
+    else
+      asset_path('placeholder.jpg')
+    end
+  end
 end
